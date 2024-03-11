0e82a5df73733724b952f6a7f205130bb932f28b
Xin Yi Koay <64830628+koayxy@users.noreply.github.com>
2024-03-10 22:34:29 -0400

    Time: 12 ms (77.67%), Space: 11.9 MB (38.64%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValid(self, s):
+        # list for open brackets count
+        stack = []
+        # define mapping for brackets
+        mapping = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            # pop top element from stack, if empty stack then assign a placeholder
+            if char in mapping:
+                top = stack.pop() if stack else '#'
+                # check if popped open bracket matches bracket mapping
+                if mapping[char] != top:
+                    return False
+            else:
+                # if char is open bracket, push to stack
+                stack.append(char)
+
+        # expect empty stack if iteration succeed
+        return not stack
