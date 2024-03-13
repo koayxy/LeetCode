eae2a9e73592e60dc2f68ce58111de13678b41c2
Xin Yi Koay <64830628+koayxy@users.noreply.github.com>
2024-03-12 23:14:04 -0400

    Time: 19 ms (90.00%), Space: 12 MB (92.50%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.py
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.py
@@ -6,26 +6,33 @@ class ListNode(object):
 
 class Solution(object):
     def removeZeroSumSublists(self, head):
+        # initialize sum of input
         prefix_sum = 0
-        # Keep track of prefix sumss and nodes
+        # store prefix sums and nodes
         prefix_sum_map = {}
-        # dummy 0 points to the head of list
+        # dummy of val 0 points to the head of list
         dummy = ListNode(0)
         dummy.next = head
         current = dummy
         
         while current:
+            # update prefix sum
             prefix_sum += current.val
+            # check if current prefix sum has second occurence
             if prefix_sum in prefix_sum_map:
-                # Remove nodes from the linked list between the two occurrences of prefix_sum
+                # remove nodes from linked list between the two occurrences of prefix_sum
                 prev = prefix_sum_map[prefix_sum].next
                 curr_sum = prefix_sum + prev.val
+                # remove nodes until current sum = prefix sum
                 while curr_sum != prefix_sum:
+                    # remove current sum, move node and update current sum
                     del prefix_sum_map[curr_sum]
                     prev = prev.next
                     curr_sum += prev.val
+                # update next pointer 
                 prefix_sum_map[prefix_sum].next = current.next
             else:
+                # store prefix sum and node if its first occurence
                 prefix_sum_map[prefix_sum] = current
             current = current.next
         
