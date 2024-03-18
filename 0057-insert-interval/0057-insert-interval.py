9ccbef7b4f9c09c60802dcbeff0ea9b763ca39c4
Xin Yi Koay <64830628+koayxy@users.noreply.github.com>
2024-03-17 22:19:10 -0400

    Time: 48 ms (79.12%), Space: 15.2 MB (73.98%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,15 +1,19 @@
 class Solution(object):
     def insert(self, intervals, newInterval):
+        # store merged intervals
         merged = []
+        # start and end of new interval
         start, end = newInterval
         
         for interval in intervals:
+            # if end of current interval is less than start of new interval & no overlaps, add current interval to merged list
             if interval[1] < start:
                 merged.append(interval)
+            # if start of new interval is greater than end of current interval & no overlaps, add new interval to merged list
             elif interval[0] > end:
                 merged.append([start, end])
                 start, end = interval
-            else:
+            else: # if there's overlaps, update start and end of new intervals
                 start = min(start, interval[0])
                 end = max(end, interval[1])
         
